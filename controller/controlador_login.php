c89422824664a4659b7284a9a66a7ec16360168a
Brayan121505 <bstorres966@soy.sena.edu.co>
2024-11-15 16:17:40 -0500

    LOGIN FUNCIONANDO
    Recordar poder cambiar el acceso a la ubicaion, ya que no igresa aun al apartado del index, se probo fue con prueba.php y funciono, determinar cual es la ruta del archivo par que ingrese de forma correcta.

---
+++ controller/controlador_login.php
@@ -0,0 +1,21 @@
+<?php
+if(!empty($_POST["btningresar"])){
+    if (!empty($_POST["usuario"]) and !empty($_POST["contrasena"])) {
+        $usuario=$_POST["usuario"];
+        $contrasena=$_POST["contrasena"];
+        $sql=$conexion->query ("SELECT * FROM usuarios WHERE usuario='$usuario' AND contrasena='$contrasena'");
+        if ($datos = $sql->fetch_object()) {
+            header("location: prueba.php");
+        } else {
+            echo("<div class= 'alert alert-danger'> Acceso denegado</div>");
+        }
+        
+    } else {
+        echo "campos vacios";
+    }
+    
+}
+?>
+
+
+
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en"> <!--begin::Head-->
 
@@ -12,25 +13,33 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous"><!--end::Fonts--><!--begin::Third Party Plugin(OverlayScrollbars)-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css" integrity="sha256-dSokZseQNT08wYEWiz5iLI8QPlKxG+TswNRD8k35cpg=" crossorigin="anonymous"><!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Third Party Plugin(Bootstrap Icons)-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="../../../dist/css/adminlte.css"><!--end::Required Plugin(AdminLTE)-->
+    <link rel="stylesheet" href="AdminPa/dist/css/adminlte.css"><!--end::Required Plugin(AdminLTE)-->
 </head> <!--end::Head--> <!--begin::Body-->
 
 <body class="login-page bg-body-secondary">
     <div class="login-box">
+        
         <div class="login-logo"> LOGIN ENFERMERIA</div> <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Ingrese sus credenciales</p>
-                <form action="conexion.php" method="post">
-                    <div class="input-group mb-3"> <input type="text" class="form-control" placeholder="Usuario" name="usuario">
+<?php
+include "model/conexion.php";
+include "controller/controlador_login.php";
+?>   
+                <form action="" method="post">
+                    <div class="input-group mb-3"> 
+                        <input type="text" class="form-control" placeholder="Usuario" id="usuario" name="usuario">
                         <div class="input-group-text"> <i class="bi bi-person-vcard-fill"></i></span> </div>
                     </div>
-                    <div class="input-group mb-3"> <input type="password" class="form-control" placeholder="Contraseña" name="contraseña">
+                    <div class="input-group mb-3"> 
+                        <input type="password" class="form-control" placeholder="Contraseña" id="contrasena" name="contrasena">
                         <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                     </div> <!--begin::Row-->
                     <div class="row">
                         <div class="col-4">
-                            <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Ingresar</button> </div>
+                        <input type="submit" name="btningresar" value="Ingresar" class="btn btn-primary">
+
                         </div> <!-- /.col -->
                     </div> <!--end::Row-->
                 </form>
