32fe24eb66a4701bbe49bb0197a9391aaf6a2843
Brayan <brayan.storres121505@gmail.com>
2024-11-24 21:18:54 -0500

    Linea model para pc casa

--- model/conexion.php
+++ model/conexion.php
@@ -1,6 +1,6 @@
 <?php
-$conexion=new mysqli("localhost","root","12345","enfermeria");
-
+//$conexion=new mysqli("localhost","root","12345","enfermeria");
+$conexion=new mysqli("localhost","root","","enfermeria");
 
 
 
